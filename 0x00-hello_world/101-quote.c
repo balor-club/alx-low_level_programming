4b72e4a95b4bb1fa71453df2b6f05d447239fbb6
Esther <kiiruesther66@gmail.com>
2022-06-30 09:11:29 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t len = strlen(str);
-	fwrite(str, len, 1, stderr)
+	fwrite(str, len, 1, stderr);
 	return (1);
 }
