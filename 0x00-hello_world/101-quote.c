bba147e040f1d66e4d1f501fb350264caf5cde57
Esther <kiiruesther66@gmail.com>
2022-06-30 06:44:46 -0700

    Use write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry file
  *
@@ -8,7 +7,6 @@
  */
 int main(void)
 {
-	fwrite(STDOUT_FILEN0, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
