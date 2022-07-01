63673aeb93ad3cd5eb854a86e11c4f926dc7fd46
Esther <kiiruesther66@gmail.com>
2022-07-01 04:03:28 -0700

    Lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(void)
+{
+	/* local variable definition */
+	char ch
+
+	for(ch = 'a' ; ch <= 'z' ; ch++) 
+	{
+		putchar(ch);
+	}
+	return (0);
+}
