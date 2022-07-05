afe2ab8a804454d319b7c5793620e586b1f82b61
Esther <kiiruesther66@gmail.com>
2022-07-05 02:34:51 -0700

    Absolut integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - absolute value of an integer
+ *
+ * Return: Always (0)
+ */
+
+int _abs(int)
+{
+	int r;
+
+	if (r < 0)
+	{
+		r = (-1) * r
+	}
+	else
+	{
+		_putchar(r);
+	}
+	
+	return (0);
+}
