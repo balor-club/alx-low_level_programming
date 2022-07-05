f693c1708e382bac9a283d747b93a303d6556204
Esther <kiiruesther66@gmail.com>
2022-07-05 01:47:11 -0700

    Ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - prints a-z ten times
+ *
+ * Return: Always (0) Success
+ */
+
+int void print_alphabet_x10(void);
+int main()
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z'; apha++)
+	{
+		putchar(alpha);
+	}
+}
+
+int main()
+{
+	repeat ('alpha', 10);
+	putchar('\n');
+
+	return (0);
+}
