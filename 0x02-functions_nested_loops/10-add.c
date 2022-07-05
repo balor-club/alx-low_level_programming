e951dfe5d1c366d81ae567c89e1fea5c3f4e7160
Esther <kiiruesther66@gmail.com>
2022-07-05 09:36:11 -0700

    Add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+# include <main.h>
+# include <stdio.h>
+
+/**
+ * main - adds two integers
+ *
+ * Return: Always (0)
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+	int n;
+
+	for (n = a + b)
+	{
+		printf("%d\n", n);
+	}
+
+	return (0);
+}
