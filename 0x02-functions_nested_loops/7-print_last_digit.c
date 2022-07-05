d6f87e87b1ad60a4bf76bcbfbf73e1202a00d1cf
Esther <kiiruesther66@gmail.com>
2022-07-05 08:19:15 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,3 +9,18 @@
 
 int print_last_digit(int)
 {
+	int nv;
+
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(nv + '0');
+		return (nv);
+	}
+	else
+	{
+		nv = n % 10;
+		_putchar(nv + '0');
+		return (nv);
+	}
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - print every minute of the day
+ *
+ * Return: Always (0)
+ */
+
+void jack_bauer(void)
+{
+	int
