06a43d6d24760a12f691ccd13dd597f500e5b1ce
Esther <kiiruesther66@gmail.com>
2022-07-06 09:16:14 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,7 +19,7 @@ void print_times_table(int n)
 			v = a * b;
 			if (v > 99)
 			{
-				_putchar(v / 100 +'0');
+				_putchar(v / 100 + '0');
 				_putchar((v / 10 % 10) + '0');
 				_putchar(v % 10 + '0');
 			}
@@ -27,7 +27,7 @@ void print_times_table(int n)
 			{
 				_putchar(' ');
 				_putchar(v / 10 + '0');
-				_putchar(v % 10 +'0');
+				_putchar(v % 10 + '0');
 			}
 			else if (b != 0)
 			{
