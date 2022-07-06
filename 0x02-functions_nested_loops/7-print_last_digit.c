97c372d09bb824894be599cae7585a499a3eabf8
Esther <kiiruesther66@gmail.com>
2022-07-06 04:33:30 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,20 +6,20 @@
  * Return: Last digit of the number
  */
 
-int print_last_digit(int)
+int print_last_digit(int nv)
 {
-	int nv;
+	int n;
 
 	if (n < 0)
 	{
 		nv = -1 * (n % 10);
 		_putchar(nv + '0');
-		return (nv);
+		return (n);
 	}
 	else
 	{
 		nv = n % 10;
 		_putchar(nv + '0');
-		return (nv);
+		return (n);
 	}
 }
