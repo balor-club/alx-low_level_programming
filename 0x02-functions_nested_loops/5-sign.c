b5a567b1a3dcd4e4a45e074939864a4546c870f5
Esther <kiiruesther66@gmail.com>
2022-07-06 03:55:33 -0700

    Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,19 @@
 
 /**
  * print_sign - prints the sign of a number
+ * @n: The integer
  *
  * Return: 1 if greater than zero. 0 if zero. -1 if less than zero
  */
 
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if ((n < '0'))
+	else if ((n < 0))
 	{
 		_putchar('-');
 		return (-1);
