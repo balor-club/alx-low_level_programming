8ba4976bed587f939a73aa180cb8e1737c1d8eb9
Esther <kiiruesther66@gmail.com>
2022-07-06 10:30:38 -0700

    Add even numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - print sum of even Fibanacci numbers
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int c;
+	long int i, j, k;
+
+	i = 1;
+	j = 2;
+	k = 0;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			total +=j;
+
+		c = j;
+		j += i;
+		i = c;
+	}
+	printf("%d\n", k);
+
+	return (0);
+}
