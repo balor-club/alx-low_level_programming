cfcea2317f32ca4d97b6e547da89626775d4d702
Esther <kiiruesther66@gmail.com>
2022-07-07 04:50:02 -0700

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+# include "main.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * isupper - checks for uppercase letters
+ * @c: character
+ * ASCII numbers
+ *
+ * Return: 1 if c is uppercase. Otherwise 0
+ */
+
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
