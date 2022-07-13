71640c94cfaf90eb8e21cb986d337793ada86624
Esther <kiiruesther66@gmail.com>
2022-07-13 09:51:04 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,13 +13,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int count = 0, count2 = 0;
 
-	if (sizeof(src) < n)
-		_putchar("");
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	_putchar(dest);
-	_putchar('\n');
-
-	return (0);
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
