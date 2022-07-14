65b0040ac6865e9b851bdc6005ae6e1051c5b5eb
Esther <kiiruesther66@gmail.com>
2022-07-14 03:14:27 -0700

    Print intyeger

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,29 @@
 
 void print_number(int n)
 {
-	if (n > 9)
+	unsigned int d, m, count;
+
+	if n < 0
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(45);
+		m = n * -1;
+	}
+	else 
+	{
+		m = n;
 	}
 
-	_putchar(n);
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
