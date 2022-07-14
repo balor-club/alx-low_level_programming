14f6658a1d88ffe0ba739f4da2948a385bc2f054
Esther <kiiruesther66@gmail.com>
2022-07-14 07:02:52 -0700

    Magic number

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/**
+ * main - add a line of code
+ *
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	p[5] = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
