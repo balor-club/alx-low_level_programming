8b5d5be8279d0d7b433c674ae4c93d5e46ea6008
Esther <kiiruesther66@gmail.com>
2022-07-14 03:33:14 -0700

    Compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,20 @@
  * @s1: First string
  * @s2: Second string
  *
- * Return: (0)
+ * Return: 0 if same, another number if not
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int i = 0, op = 0;
 
-	return (0);
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
 }
