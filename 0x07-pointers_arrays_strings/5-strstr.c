82d4f9257bacb701a2efbe5a556a8e0c7abf2cfb
Esther <kiiruesther66@gmail.com>
2022-07-18 07:02:18 -0700

    Locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	if (needle[0] == '\0')
 		return (haystack);
 
-	for (i = 0; *(haystack[i]) != '\0'; i++)
+	for (i = 0; (haystack[i]) != '\0'; i++)
 	{
 		if (haystack[i] == needle[0])
 		{
