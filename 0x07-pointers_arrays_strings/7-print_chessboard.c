3832500880941cfb97b93e2ebcffeaf02bc3ba7d
Esther <kiiruesther66@gmail.com>
2022-07-18 06:23:09 -0700

    Chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+# include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: Characters
+ *
+ * Return: no return
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
+}
