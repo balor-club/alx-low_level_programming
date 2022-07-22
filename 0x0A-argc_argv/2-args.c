9003696453d0371cc06c4e6bfb9b2e602cff6ac5
Esther <kiiruesther66@gmail.com>
2022-07-22 11:23:37 -0700

    Print all

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: argument count
+ * @argv: array of pointers to CLI argurment
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	
+	return (0);
+}
