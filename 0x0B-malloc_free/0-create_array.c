27f9a20c43bd12fcee1346b80cc57b9e3df10f59
Esther <kiiruesther66@gmail.com>
2022-07-25 08:43:34 -0700

    Array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	
-	cr = malloc(size of(c) * size);
+	cr = malloc(sizeof(c) * size);
 
 	if (cr == NULL)
 		return (NULL);
