d884e410c127863d7156dbe40bdfd725eafe7a40
Esther <kiiruesther66@gmail.com>
2022-07-28 08:53:38 -0700

    Concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char s;
 
 	if (s1 == NULL)
-		s2 = "";
-	if (s2 == NULL)
 		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	len1 = 0;
 	len2 = 0;
