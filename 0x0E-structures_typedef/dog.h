2940e7671e56d1084ad69ca39548b29a1184f185
Esther <kiiruesther66@gmail.com>
2022-08-09 04:45:41 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * struct dog - declares types
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: nothing
+ */
+
+struct user
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
