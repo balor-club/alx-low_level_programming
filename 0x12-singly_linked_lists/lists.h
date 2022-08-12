b5e7af2f9235d8f7d14db89edc433252115c49e4
Esther <kiiruesther66@gmail.com>
2022-08-12 02:50:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
