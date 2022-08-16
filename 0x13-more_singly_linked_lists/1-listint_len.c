3c0158b6d0a6eacf50a4584e872ec60c732fcfc9
Esther <kiiruesther66@gmail.com>
2022-08-16 06:02:12 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+# include "lists.h"
+
+/**
+ * listint_len - returns number of elements in a linked list
+ * @h: head of linked list
+ *
+ * Return: number of elements in a linked list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes;
+	nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+
+	return (nnodes);
+}
