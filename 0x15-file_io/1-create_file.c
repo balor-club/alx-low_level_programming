2fb7cb6a1508d7eefd0d56b0d55f5ecc1d6731d9
Esther <kiiruesther66@gmail.com>
2022-08-22 10:02:20 -0700

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+# include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of file
+ * @text_content: NULL string
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, num = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (num = 0; text_content[num];)
+
+			num++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, num);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
