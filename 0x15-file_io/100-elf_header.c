08216d74b628db9de38020fa597478614726ccc3
Esther <kiiruesther66@gmail.com>
2022-08-22 12:04:46 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,10 @@
-# include "main.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 # include <elf.h>
 
 /**
