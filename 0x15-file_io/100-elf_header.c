52ab8304ad2a44f6a6cd61e15be233c890dfb8e5
Esther <kiiruesther66@gmail.com>
2022-08-22 12:12:03 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -13,7 +13,7 @@
  *
  */
 
-voide print_addr(char *ptr)
+void print_addr(char *ptr)
 {
 	int i;
 	int begin;
@@ -33,11 +33,11 @@ voide print_addr(char *ptr)
 			else if (ptr[i] < 0)
 				printf("%x", 256 + ptr[i]);
 		}
-		
+
 		if (ptr[7] == 6)
 			printf("00");
 	}
-	
+
 	if (sys == '2')
 	{
 		begin = 26;
@@ -102,7 +102,7 @@ void print_osabi(char *ptr)
 		printf("UNIX - Solaris\n");
 	else
 		printf("<unknown: %x>\n", osabi);
-	
+
 	printf("  ABI Version:                       %d\n", ptr[8]);
 }
 
@@ -118,10 +118,10 @@ void print_version(char *ptr)
 	int version = ptr[6];
 
 	printf("  Version:                           %d", version);
-	
+
 	if (version == EV_CURRENT)
 		printf(" (current)");
-	
+
 	printf("\n");
 }
 
@@ -135,12 +135,12 @@ void print_version(char *ptr)
 void print_data(char *ptr)
 {
 	char data = ptr[5];
-	
+
 	printf("  Data:                              2's complement");
-	
+
 	if (data == 1)
 		printf(", little endian\n");
-	
+
 	if (data == 2)
 		printf(", big endian\n");
 }
@@ -155,12 +155,12 @@ void print_data(char *ptr)
 void print_magic(char *ptr)
 {
 	int bytes;
-	
+
 	printf("  Magic:  ");
-	
+
 	for (bytes = 0; bytes < 16; bytes++)
 		printf(" %02x", ptr[bytes]);
-	
+
 	printf("\n");
 }
 
@@ -174,18 +174,18 @@ void print_magic(char *ptr)
 void check_sys(char *ptr)
 {
 	char sys = ptr[4] + '0';
-	
+
 	if (sys == '0')
 		exit(98);
-	
+
 	printf("ELF Header:\n");
 	print_magic(ptr);
-	
+
 	if (sys == '1')
 		printf("  Class:                             ELF32\n");
 	if (sys == '2')
 		printf("  Class:                             ELF64\n");
-	
+
 	print_data(ptr);
 	print_version(ptr);
 	print_osabi(ptr);
@@ -206,10 +206,10 @@ int check_elf(char *ptr)
 	char E = ptr[1];
 	char L = ptr[2];
 	char F = ptr[3];
-	
+
 	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
 		return (1);
-	
+
 	return (0);
 }
 
@@ -225,37 +225,37 @@ int main(int argc, char *argv[])
 {
 	int fd, ret_read;
 	char ptr[27];
-	
+
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
-	
+
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
 		dprintf(STDERR_FILENO, "Err: file can not be open\n");
 		exit(98);
 	}
-	
+
 	lseek(fd, 0, SEEK_SET);
 	ret_read = read(fd, ptr, 27);
-	
+
 	if (ret_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
 		exit(98);
 	}
-	
+
 	if (!check_elf(ptr))
 	{
 		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
 		exit(98);
 	}
-	
+
 	check_sys(ptr);
 	close(fd);
-	
+
 	return (0);
 }
